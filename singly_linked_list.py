98935cf20d1971f94dc0f4027b28e22d173ae0dd
yoonyeolhoon <yyhoooon@gmail.com>
2024-05-04 22:34:22 +0900

    commit : singly LL update (erase method and insert method)

--- singly_linked_list.py
+++ singly_linked_list.py
@@ -17,7 +17,12 @@ class singly_linked_list:
             while cur.next:
                 cur = cur.next
             cur.next = new_node
-            new_node.before = cur
+
+    def prepend(self, data):
+        new_node = node(data)
+        cur = self.head
+        self.head = new_node
+        new_node.next = cur
 
     def length(self):
         cur = self.head
@@ -31,9 +36,9 @@ class singly_linked_list:
         elements = []
         cur_node = self.head
         while cur_node:
-            elements.append(cur_node.data)
+            elements.append(str(cur_node.data))
             cur_node = cur_node.next
-        print(elements)
+        print(" -> ".join(elements))
 
     def get(self, index):
         if index >= self.length():
@@ -47,31 +52,141 @@ class singly_linked_list:
             cur_node = cur_node.next
             cur_index += 1
 
-    def erase(self, index):
-        if index >= self.length():
-            print("ERROR: 'Erase' Index out of range!")
-            return
+    def get_data(self, data):
         cur_index = 0
         cur_node = self.head
         while cur_node:
-            if cur_index == index:
-                if cur_node.before:
-                    cur_node.before.next = cur_node.next
-                if cur_node.next:
-                    cur_node.next.before = cur_node.before
-                if cur_index == 0:
-                    self.head = cur_node.next
+            if cur_node.data == data:
+                print("data : {0} index : {1}".format(data, cur_index))
                 return
             cur_node = cur_node.next
             cur_index += 1
 
+        print("there is no data about {}".format(data))
+        return
+
+    def erase(self, index):
+        if index >= self.length():
+            print("ERROR: 'Erase' Index out of range!")
+            return
+
+        if index == 0:
+            self.head = self.head.next
+            return
+
+        cur_index = 0
+        cur_node = self.head
+        while cur_node:
+            if cur_index == index - 1:
+                cur_node.next = cur_node.next.next
+                return
+            cur_node = cur_node.next
+            cur_index += 1
+
+    def erase_prev(self, cur_data):
+        cur_node = self.head
+        prev_node = None
+        while cur_node:
+            if cur_node.data == cur_data:
+                if prev_node:
+                    if prev_node == self.head:
+                        self.head = cur_node
+                    else:
+                        prev_node.next = cur_node.next
+                    return
+                else:
+                    print("Cannot erase previous node.")
+                    return
+            prev_node = cur_node
+            cur_node = cur_node.next
+
+        print("No node with data '{}' found.".format(cur_data))
+        return
+
+    def erase_next(self, cur_data):
+        cur_node = self.head
+        while cur_node:
+            if cur_node.data == cur_data:
+                if cur_node.next:
+                    cur_node.next = cur_node.next.next
+                    return
+                else:
+                    print("Cannot erase next node.")
+                    return
+            cur_node = cur_node.next
+
+        print("No node with data '{}' found.".format(cur_data))
+        return
+
+    def insert(self, index, insert_data):
+        if index >= self.length():
+            print("ERROR: 'Get' Index out of range!")
+            return None
+        if index == 0:
+            self.prepend(insert_data)
+            return
+        new_node = node(insert_data)
+        cur_node = self.head
+        cur_index = 0
+        prev_node = None
+        while cur_node:
+            if cur_index == index:
+                prev_node.next = new_node
+                new_node.next = cur_node
+                return
+            prev_node = cur_node
+            cur_node = cur_node.next
+            cur_index += 1
+
+    def insert_prev(self, cur_data, insert_data):
+        new_node = node(insert_data)
+        cur_node = self.head
+        prev_node = None
+        while cur_node:
+            if cur_node.data == cur_data:
+                new_node.next = cur_node
+                if prev_node:
+                    prev_node.next = new_node
+                else:
+                    self.head = new_node
+                return
+            prev_node = cur_node
+            cur_node = cur_node.next
+
+        print("No node with data {} found.".format(cur_data))
+        return
+
+    def insert_next(self, cur_data, insert_data):
+        new_node = node(insert_data)
+        cur_node = self.head
+        while cur_node:
+            if cur_node.data == cur_data:
+                new_node.next = cur_node.next
+                cur_node.next = new_node
+                return
+            cur_node = cur_node.next
+        print("No node with data {} found.".format(cur_data))
+        return
+
+
+
+    def main(self):
+        self.display()
+        self.append(1)
+        self.append(4)
+        self.append("a")
+        self.display()
+        "erase 1 index "
+        self.prepend(5)
+        self.get_data("a")
+        self.insert_prev(1, 3)
+        self.insert_next(1, 8)
+        self.insert(1, 10)
+        self.display()
+        self.erase_next(10)
+        self.erase_prev(10)
+        self.display()
+
 
 test_list = singly_linked_list()
-test_list.display()
-test_list.append(1)
-test_list.append(4)
-test_list.append("a")
-test_list.display()
-"erase 1 index "
-test_list.erase(1)
-test_list.display()
+test_list.main()
